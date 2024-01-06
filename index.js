0437a7bf4611f549c0f8cb4de557ba36a8e3e0d6
shivammishra349 <shivam9.mishra1999@gmail.com>
2024-01-06 11:23:42 +0530

    project done with style

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let totalprice=0
+let totalprice =0;
+let prices=[]
 function crud(event){
     event.preventDefault();
     let price=event.target.num.value;
@@ -9,30 +10,43 @@ function crud(event){
         price:price,
         name:name
     }
-    
-    axios.post("https://crudcrud.com/api/5f40a6aab5aa4f5ba11169c46baca321/ecom",obj)
-    .then((res)=>{
-        showuser(res.data)
-        
-        totalprice +=Number(price);
-        displayTotalPrice(totalprice);
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
-}
-axios.get("https://crudcrud.com/api/5f40a6aab5aa4f5ba11169c46baca321/ecom")
-.then((res)=>{
-    totalprice = res.data.reduce((acc, item) => acc + item.price, 0);
-        displayTotalPrice(totalprice);
-    for(let i=0;i<res.data.length;i++)
-    {
-        showuser(res.data[i]) 
+   async function post(url,obj){
+    try{
+        let res=  await axios.post(url,obj)
+        showuser(res.data);
+
+        prices.push(Number(price));
+        console.log(prices)
+        totalprice=calculatePrice(prices)
+        displayTotalPrice(totalprice)
     }
-})
-.catch((err)=>{
-    console.log(err)
-})
+    catch(err){
+        console.log(err)
+    }
+}
+post("https://crudcrud.com/api/bee6feeca4104391aa305f9ed7d5cb76/ecom",obj)
+    
+}
+
+async function get(){
+    try{
+        let res= await axios.get("https://crudcrud.com/api/bee6feeca4104391aa305f9ed7d5cb76/ecom")
+        for(let i=0;i<res.data.length;i++){
+            showuser(res.data[i])
+            prices.push(Number(res.data[i].price))
+        }
+        totalprice=calculatePrice(prices)
+        displayTotalPrice(totalprice)
+        
+    }
+    catch(err){
+        console.log(err)    
+    }
+    
+}
+get()
+
+
 
 function showuser(obj){
     let parent=document.getElementById('listItem');
@@ -42,34 +56,45 @@ function showuser(obj){
     let deletebut=document.createElement('input');
     deletebut.type="button";
     deletebut.value="Delete Product"
+    deletebut.className="btn btn-dark";
 
     child.appendChild(deletebut);
     parent.appendChild(child)
     
-   
-    deletebut.addEventListener('click',deleteuser)
+    deletebut.addEventListener('click',deleteuser);
+    
+    async function deleteuser(){
+        try{
+            await axios.delete(`https://crudcrud.com/api/bee6feeca4104391aa305f9ed7d5cb76/ecom/${obj._id}`);
+            parent.removeChild(child)
+            let index = prices.indexOf(Number(obj.price));
+            if (index > -1) {
+                prices.splice(index, 1);
+            }
+            totalprice = calculatePrice(prices);
+            displayTotalPrice(totalprice)
 
-    function deleteuser(){
-        axios.delete(`https://crudcrud.com/api/5f40a6aab5aa4f5ba11169c46baca321/ecom/${obj._id}`)
-        .then(()=>{
-            parent.removeChild(child);
-            totalprice -= obj.price;
-            displayTotalPrice(totalprice);
-        })
-        .catch((err)=>{
+        }
+        catch(err){
             console.log(err)
-        })
-    }
+        }
+    } 
+    
 
 }
+
+
+function calculatePrice(values){
+    console.log(values)
+    let total=0;
+    for(let i=0;i<values.length;i++){
+       total +=values[i]
+    }
+    return total
+}
+
 
 function displayTotalPrice(total) {
     let totalPriceElement = document.getElementById('TotalCost');
-    totalPriceElement.textContent = "Total Value worth of Products:" + total; 
+    totalPriceElement.textContent = "Total Value worth of Products:Rs " + total; 
 }
-
-
-
-
-
-
